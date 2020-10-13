ee46af919b790bbd2c63e4eacccc41c6c81ce357
nehemiasq <nquinto7070@gmail.com>
2020-10-12 21:59:53 -0500

    Implementando model Login

---
+++ app/Models/LoginModel.php
@@ -0,0 +1,26 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class LoginModel extends Model
+{
+    protected $table         = 'login';
+    protected $primaryKey    = 'id_usuario';
+    protected $allowedFields = [
+        'id_usuario', 'nombre_usuario', 'clave_usuario'
+    ];
+
+    function index(){
+
+      return $this->asArray()
+              ->select('*')
+              ->findAll();  //ejecuta el query
+    }
+
+    public function insertarPosteModel($data){
+
+    	return $this->db->table($this->table)->insert($data); 
+  
+    }
+    
+}
