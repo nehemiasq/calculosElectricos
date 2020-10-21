a77e441b6a10a391923bb6cd2c7171eb59ff8bd3
nehemiasq <nquinto7070@gmail.com>
2020-10-20 22:50:47 -0500

    Asignando login como ventana principal

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,7 +4,7 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('vistaPrincipal');
+		return view('vistaLogin');
 	}
 
 	//--------------------------------------------------------------------
