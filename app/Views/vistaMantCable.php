dab5fd7295021a323d3065bf7f0d4ba36beef810
nehemiasq <nquinto7070@gmail.com>
2020-11-04 22:15:25 -0500

    Reporte pdf implementado

--- app/Views/vistaMantCable.php
+++ app/Views/vistaMantCable.php
@@ -25,10 +25,11 @@
                 <a href="<?php echo base_url()?>/public/ControllerPrincipal" class="btn btn-primary" name="btn_buscar" value="Mantenimiento Poste">Menu Principal</a>
 
                 <button onclick="exportTableToExcel('tblData')">Exportar excel</button>
+                <button onclick="demoFromHTML()">Exportar pdf</button>
 
                 <!--<a href="insert.php" class="btn btn__nuevo">Nuevo</a>-->
   </div>
-  
+  <div id="customers" name="customers">
   <table id="tblData" class="table table-striped">
   <thead>
     <tr>
@@ -42,9 +43,11 @@
    
   </tbody>
 </table>
+</div>
 </html>
 
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="../assets/js/jspdf.debug.js"></script>
 <script type="text/javascript">
 
 listarCables(); //Creo mi método
@@ -139,5 +142,45 @@ function eliminarCable(param_idCable){
         downloadLink.click();
     }
 }
+
+ function demoFromHTML() {
+               var pdf = new jsPDF('l', 'pt', 'a4');
+               // source can be HTML-formatted string, or a reference
+               // to an actual DOM element from which the text will be scraped.
+               source = $('#customers')[0];
+
+               // we support special element handlers. Register them with jQuery-style 
+               // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+               // There is no support for any other type of selectors 
+               // (class, of compound) at this time.
+               specialElementHandlers = {
+                   // element with id of "bypass" - jQuery style selector
+                   '#bypassme': function(element, renderer) {
+                       // true = "handled elsewhere, bypass text extraction"
+                       return true
+                   }
+               };
+               margins = {
+                   top: 80,
+                   bottom: 60,
+                   left: 100,
+                   width: 522
+               };
+               // all coords and widths are in jsPDF instance's declared units
+               // 'inches' in this case
+               pdf.fromHTML(
+                       source, // HTML string or DOM elem ref.
+                       margins.left, // x coord
+                       margins.top, {// y coord
+                           'width': margins.width, // max width of content on PDF
+                           'elementHandlers': specialElementHandlers
+                       },
+               function(dispose) {
+                   // dispose: object with X, Y of the last line add to the PDF 
+                   //          this allow the insertion of new lines after html
+                   pdf.save('Reporte_Cable.pdf');
+               }
+               , margins);
+           }
 
 </script>
